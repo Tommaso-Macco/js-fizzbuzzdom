38b87520dbbe4760c5a44e3deca1181654699db9
Tommaso <tommaso.macco@gmail.com>
2021-10-25 20:33:19 +0200

    square 1 to 100 (nostyle)

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let squareCont = document.getElementById("containersquare")
 
-for (let i = 1; i <= 100; i++) {
-    const square = "<div class;``
-    
+for (let i=1; i <=100; i++) {
+    squareCont.innerHTML += `<div class="square">${i}</div>`
 }
+
+console.log(square);
