e5a82adefb5851c4f6b7d6361a88f95f8e8677c1
Tommaso <tommaso.macco@gmail.com>
2021-10-26 09:51:14 +0200

    script done

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let squareCont = document.getElementById("containersquare")
 
 for (let i=1; i <=100; i++) {
-    squareCont.innerHTML += `<div class="square">${i}</div>`
+
+    if( ( i % 3 === 0 ) && ( i % 5 === 0 ) ){
+        squareCont.innerHTML += `<div class="square fizzbuzz">fizzbuzz</div>`
+    }
+    else if( i % 3 == 0 ){
+        squareCont.innerHTML += `<div class="square fizz">Fizz</div>`
+    }
+    else if( i % 5 == 0 ){
+        squareCont.innerHTML += `<div class="square buzz">Buzz</div>`
+    }
+    else {
+        squareCont.innerHTML += `<div class="square">${i}</div>`
+    }
+
+
 }
+
 
 console.log(square);
